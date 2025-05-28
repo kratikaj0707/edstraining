9238d058fd95a686adec860204fb3ca8812bb606
kratikaj0707 <kratikaj@adobe.com>
2025-05-28 11:49:12 +0530

    ch

---
+++ blocks/section/section.js
@@ -0,0 +1,11 @@
+export default function decorate(block){
+    const img= block.queryselector('img');
+    const text=block.queryselector('text');
+    block.classList.add('sectionhome');
+    const content = document.createElement('sectionhome');
+    content.className='section-content';
+    if (text) content.appendChild(Object.assign(document.createElement('h1'), { innerHTML: text.innerHTML }));
+    block.innerHTML = '';
+    if (img) block.appendChild(img);
+    block.appendChild(content);
+}
